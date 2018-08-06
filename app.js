8765648fac4487ad0d1013d66015033f7db7e99a
DESKTOP-0K1M4TV\Kenshin <hung.dt8796@gmail.com>
2018-08-06 11:40:25 +0700

    6/8/2018

--- app.js
+++ app.js
@@ -5,8 +5,4 @@ var path = require('path');
 // viewed at http://localhost:8080
 app.use(express.static(__dirname + '/' ));
 
-app.post('/', function( req ,res){
-    res.send("Success");
-});
-
 app.listen(8080);
--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 let audioCtx = new AudioContext();
 let destination = audioCtx.createMediaStreamDestination();
 let myAudio = document.querySelector('audio#song');
+let myVideo = document.querySelector('video#video');
 const recordButton = document.querySelector('button#record');
 const playButton = document.querySelector('button#play');
+const downloadButton = document.querySelector('button#download');
 
 let streamAudio;
 let mediaRecorder;
 let recordedBlobs;
 let sourceMusic = audioCtx.createMediaElementSource(myAudio);
+let sourceVideo = audioCtx.createMediaElementSource(myVideo);
+
+let recordedAudio = document.querySelector('#recordedAudio');
 sourceMusic.connect(audioCtx.destination);
+sourceVideo.connect(audioCtx.destination);
+
 recordButton.addEventListener('click', () => {
     if (recordButton.textContent === 'Start Recording') {
       startRecording();
@@ -16,14 +23,18 @@ recordButton.addEventListener('click', () => {
       stopRecording();
       recordButton.textContent = 'Start Recording';
       playButton.disabled = false;
+      downloadButton.disabled = false;
     }
   });
-
+playButton.addEventListener('click', () => {
+    recordedAudio.play();
+})
 function startRecording () {
     recordedBlobs = [];
-    let stream1 = audioCtx.createMediaStreamSource(streamAudio);
+    let pcAudioStream = audioCtx.createMediaStreamSource(streamAudio);
     sourceMusic.connect(destination);
-    stream1.connect(destination);
+    sourceVideo.connect(destination);
+    pcAudioStream.connect(destination);
     try {
         mediaRecorder = new MediaRecorder(destination.stream, {});
     } catch (e) {
@@ -50,23 +61,20 @@ function handleDataAvailable(e) {
 function stopRecording() {
     mediaRecorder.stop();
     console.log('Media Recorder stopped');
-    const blob = new Blob(recordedBlobs, {type: 'audio/ogg'});
+    const blob = new Blob(recordedBlobs, {type: 'audio/mp3'});
     console.log('recordedBlobs: ', recordedBlobs);
     console.log('blob: ', blob)
     const url = URL.createObjectURL(blob);
-    let recordedAudio = document.createElement('audio');
-    let recordedSource = document.createElement('source');
-    recordedAudio.appendChild(recordedSource);
-    document.querySelector('body').appendChild(recordedAudio);
-    recordedAudio.controls = true;
-    recordedSource.src = url;
+    recordedAudio.src = url;
+    const downloadLink = downloadButton.querySelector('a');
+    downloadLink.href = url;
+    downloadLink.download = 'test.mp3';
 }
 
 function handleSuccess(stream) {
     recordButton.disabled = false;
     streamAudio = stream;
-    console.log(audioCtx.destination)
-    console.log(destination)
+    console.log("Successfully access to microphone");
 }
 
 navigator.mediaDevices.getUserMedia({audio:true})
