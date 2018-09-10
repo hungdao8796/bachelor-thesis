3b99a90ddc3620af901cf09c0ac16e1e36ccd45b
DESKTOP-0K1M4TV\Kenshin <hung.dt8796@gmail.com>
2018-09-10 09:57:53 +0700

    latest version

--- js/main.js
+++ js/main.js
@@ -18,6 +18,7 @@ musicVolume.addEventListener('click', function (e) {
     sourceMusicGainNode.gain.value = musicVolume.value;
 });
 
+let pcAudioStream;
 let streamAudio;
 let mediaRecorder;
 let recordedBlobs;
@@ -29,7 +30,8 @@ let recordedAudio = document.querySelector('#recordedAudio');
 sourceMusic.connect(sourceMusicGainNode);
 sourceMusicGainNode.connect(audioCtx.destination);
 sourceVideo.connect(audioCtx.destination);
-
+sourceMusic.connect(destination);
+sourceVideo.connect(destination);
 callButton.addEventListener('click', () => {
     if (callButton.textContent.trim() === 'Start Calling') {
         startCalling();
@@ -43,9 +45,6 @@ recordButton.addEventListener('click', () => {
       startRecording();
     } else {
       stopRecording();
-      recordButton.textContent = 'Start Recording';
-      playButton.disabled = false;
-      downloadButton.disabled = false;
     }
 });
 
@@ -53,12 +52,24 @@ playButton.addEventListener('click', () => {
     recordedAudio.play();
 });
 
+downloadButton.addEventListener('click', () => {
+    const blob = new Blob(recordedBlobs, {type: 'audio/ogg'});
+    const url = window.URL.createObjectURL(blob);  
+    const a = document.createElement("a");  
+     a.style.display ="none";  
+     a.href = url;  
+     a.download = "test";  
+     document.body.appendChild(a);  
+     a.click();  
+     setTimeout(() => {  
+         document.body.removeChild(a);  
+         window.URL.revokeObjectURL(url);  
+     }, 100);
+});
+
 function startRecording () {
     recordedBlobs = [];
-    let pcAudioStream = audioCtx.createMediaStreamSource(streamAudio);
-    sourceMusic.connect(destination);
-    sourceVideo.connect(destination);
-    pcAudioStream.connect(destination);
+    
     try {
         mediaRecorder = new MediaRecorder(destination.stream, {});
     } catch (e) {
@@ -73,38 +84,42 @@ function startRecording () {
 }
 
 function handleStop(e) {
-  console.log('Recorder stopped: ', e);    
+    console.log('Recorder stopped: ', e);   
+    const blob = new Blob(recordedBlobs, {type: 'audio/ogg'});
+    console.log('recordedBlobs: ', recordedBlobs);
+    console.log('blob: ', blob)
+    const url = URL.createObjectURL(blob);
+    recordedAudio.src = url;
 }
 
 function handleDataAvailable(e) {
     if(e.data && e.data.size > 0) {
         recordedBlobs.push(e.data);
+        console.log(e.data);
     }
 }
 
 function stopRecording() {
     mediaRecorder.stop();
     console.log('Media Recorder stopped');
-    const blob = new Blob(recordedBlobs, {type: 'audio/mp3'});
-    console.log('recordedBlobs: ', recordedBlobs);
-    console.log('blob: ', blob)
-    const url = URL.createObjectURL(blob);
-    recordedAudio.src = url;
-    const downloadLink = downloadButton.querySelector('a');
-    downloadLink.href = url;
-    downloadLink.download = 'test.mp3';
+    
+    recordButton.textContent = 'Start Recording';
+    playButton.disabled = false;
+    downloadButton.disabled = false;
 }
-
 function handleSuccess(stream) {
     recordButton.disabled = false;
     streamAudio = stream;
     console.log("Successfully access to microphone");
+    pcAudioStream = audioCtx.createMediaStreamSource(streamAudio);
+    pcAudioStream.connect(destination);
 }
 
 function startCalling() {
     navigator.mediaDevices.getUserMedia({audio:true})
     .then(handleSuccess)
     .catch((e)=>{
+        document.getElementById('err').textContent = e.message;
         console.log(e)
     })
     callButton.textContent = 'Stop Calling';
